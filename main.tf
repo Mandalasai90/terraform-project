d3dc957edd68c5e6269fce6f68794ddd47420a23
Mandalasai90 <126144202+Mandalasai90@users.noreply.github.com>
2023-12-13 13:01:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIARSPNELGYCJEVYJ4K"
-secret_key = "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"
+access_key = "AKIATSCYNUGUIACTIX23"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0d81306eddc614a45"
+  ami             = "ami-02a2af70a66af6dfb "
   instance_type   = "t2.micro"
-  key_name        = "rmk8s"
+  key_name        = "docker"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1a"
   user_data       = <<EOF
@@ -27,7 +27,7 @@ EOF
 resource "aws_instance" "two" {
   ami             = "ami-0d81306eddc614a45"
   instance_type   = "t2.micro"
-  key_name        = "rmk8s"
+  key_name        = "docker"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1b"
   user_data       = <<EOF
